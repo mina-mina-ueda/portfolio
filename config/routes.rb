b65d631a6da5b403687e9ffea2f3769ef4b83760
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-12 15:35:04 +0000

    routing[Add]

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,27 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  #管理者側
+  namespace :admin do
+    root to: 'homes#top'
+    resources :customers, only[:index]
+    resources :posts, only[:index, :show, :edit, :update]
+    resources :event_podts, only[:edit, :update, :index]
+    resources :responses, only[:index, :new, :create]
+  end
+
+  #顧客側
+  scope module: :customer do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    resources :customers, only[:show, :update] do
+      get 'unsubscribe' => 'customers#unsubscribe'
+      patch 'withdraw' => 'customers#withdraw'
+    end
+    resources :posts, except[:destroy, :edit, :update]
+    get 'posts/thanks' => 'posts#thanks'
+    resources :event_posts, only[:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
