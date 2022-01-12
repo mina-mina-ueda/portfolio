e6405fbbd662834932e1ea4f86c15c87ebef42d6
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-12 07:19:19 +0000

    controller[Add]

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -2,6 +2,12 @@ class Customer::CustomersController < ApplicationController
   def show
   end
 
+  def update
+  end
 
+  def withdraw
+  end
 
+  def unsubscribe
+  end
 end
--- app/controllers/customer/posts_controller.rb
+++ app/controllers/customer/posts_controller.rb
@@ -1,2 +1,16 @@
 class Customer::PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def thanks
+  end
 end
