e7b54a4b95cf46e59dafab9e81e4406380f878fe
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-13 06:50:26 +0000

    routing[Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,33 @@
 Rails.application.routes.draw do
 
   devise_for :admin, controllers: {
-    sessions: "admin/sessions"
+    sessions: 'admin/sessions'
   }
 
   devise_for :customers, controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
+    registrations: 'customers/registrations',
+    sessions: 'customers/sessions'
   }
 
   #管理者側
   namespace :admin do
     root to: 'homes#top'
-    resources :customers, only[:index]
-    resources :posts, only[:index, :show, :edit, :update]
-    resources :event_posts, only[:edit, :update, :index]
-    resources :responses, only[:index, :new, :create]
+    resources :customers, only: [:index]
+    resources :posts, only: [:index, :show, :edit, :update]
+    resources :event_posts, only: [:edit, :update, :index]
+    resources :responses, only: [:index, :new, :create]
   end
 
   #顧客側
   scope module: :customer do
     root to: 'homes#top'
     get 'about' => 'homes#about'
-    resource :customers, only[:show, :update]do
+    resources :customers, only: [:show, :update]
       get 'unsubscribe' => 'customers#unsubscribe'
       patch 'withdraw' => 'customers#withdraw'
-    end
-    resources :posts, except[:destroy, :edit, :update]
+    resources :posts, except: [:destroy, :edit, :update]
     get 'posts/thanks' => 'posts#thanks'
-    resources :event_posts, only[:new, :create]
+    resources :event_posts, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
