e52dc7cf4a04397ebd169ad3e43068fc916a4a55
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-15 12:13:43 +0000

    post[Add]

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,8 +1,12 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @post = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -10,4 +14,10 @@ class Admin::PostsController < ApplicationController
 
   def update
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :categoey, :customer_id)
+  end
+
 end
--- app/controllers/customer/posts_controller.rb
+++ app/controllers/customer/posts_controller.rb
@@ -1,4 +1,6 @@
 class Customer::PostsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
   end
 
@@ -6,11 +8,23 @@ class Customer::PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.customer_id = current_customer.id
+    @post.save!
+    redirect_to posts_path
   end
 
   def thanks
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category, :customer_id)
+  end
+
 end
