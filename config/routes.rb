c2328f9fbaa61ec41f472af29c90101e9d7e0328
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-15 05:50:42 +0000

    routing[Add]

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   }
 
   devise_for :customers, skip: [:passwords], controllers: {
-    registrations: 'customers/registrations',
-    sessions: 'customers/sessions'
+    registrations: 'customer/registrations',
+    sessions: 'customer/sessions'
   }
 
   #管理者側
