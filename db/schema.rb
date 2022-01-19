847266876925d08b687dcef0a853c18c67095b0c
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-19 11:45:52 +0000

    event_post[Add]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_045743) do
+ActiveRecord::Schema.define(version: 2022_01_19_114343) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_045743) do
     t.date "period"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "event_id"
   end
 
   create_table "events", force: :cascade do |t|
