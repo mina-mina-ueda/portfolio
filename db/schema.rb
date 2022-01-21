81e9e2f37f22234f7b585cbd91a98d8f6206b80c
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-21 08:27:38 +0000

    contact[Add]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_185027) do
+ActiveRecord::Schema.define(version: 2022_01_21_055046) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2022_01_20_185027) do
     t.string "icon_id"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "phone_number", null: false
+    t.text "message", null: false
+    t.integer "subject", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
