0d26167778ac75e4c0ccdb85f344d4d2ad3ee36f
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-24 12:56:13 +0000

    [Add]

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -2,15 +2,15 @@ class Customer::FavoritesController < ApplicationController
   before_action :authenticate_customer!
 
   def create
-    favorite = current_customer.favorites.new(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    favorite = current_customer.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_to posts_path
   end
 
   def destroy
-    favorite = current_customer.favorites.find_by(post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    favorite = current_customer.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_to posts_path
   end
 
 end
