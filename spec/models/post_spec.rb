325386cb5ea031716746fc3fbd4b3891741ed210
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2022-01-26 16:24:50 +0000

    [Add]

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,13 +2,23 @@
 
 require 'rails_helper'
 
-RSpec.describe Response, "モデルに関するテスト", type: :model do
+RSpec.describe Post, "モデルに関するテスト", type: :model do
 
   context "空白のバリデーションチェック" do
-	  it "responseが空白の場合にバリデーションチェックされ保存できない" do
-	    response = Response.new(post_id: '', admin_id: '', response:'hoge')
-	      expect(response).to be_invalid
+	  it "titleが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+	    post = Post.new(title: 'hoge', body: '', category: '')
+	    expect(post).to be_invalid
+	    expect(post.errors[:post]).to be_invalid("can't be blank")
+	  end
+	  it "bodyが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+	    post = Post.new(title: '', body: 'body', category: '')
+	    expect(post).to be_invalid
+	    expect(post.errors[:body]).to be_invalid("can't be blank")
+	  end
+	  it "categoryが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+	    post = Post.new(title: '', body: '', category: 'hoge')
+	    expect(post).to be_invalid
+	    expect(post.errors[:category]).to be_invalid("can't be blank")
 	  end
 	end
-
 end
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Response, "モデルに関するテスト", type: :model do
   context "空白のバリデーションチェック" do
 	  it "responseが空白の場合にバリデーションチェックされ保存できない" do
 	    response = Response.new(post_id: '', admin_id: '', response:'hoge')
-	      expect(response).to be_invalid
+	    expect(response).to be_invalid
 	  end
 	end
 
